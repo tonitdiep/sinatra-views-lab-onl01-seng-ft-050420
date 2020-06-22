70dea166a9e43012f45915d1bc51c527d57cca50
Toni  Diep <adiep123@gmail.com>
2020-06-22 00:50:32 +0000

    Done.

--- app.rb
+++ app.rb
@@ -4,5 +4,16 @@ class App < Sinatra::Base
 		erb :index
 	end
 
-
+  get '/hello' do
+    erb :hello
+  end
+  
+  get '/goodbye' do 
+    erb :goodbye
+  end
+  
+  # get "/date" do
+  #   erb :today
+  #   (Date.strftime("%A, %B %d, %Y"))
+  # end
 end
